3ca042d9cdb5f9a50f379e6e86f43b8e3b2f0d88
Dincho Todorov <dincho@users.noreply.github.com>
2021-03-31 16:08:13 +0300

    Split backup nodes

--- main.tf
+++ main.tf
@@ -286,6 +286,50 @@ module "aws_deploy-main_backup-eu-north-1" {
   }
 }
 
+module "aws_deploy-main_backup_full-eu-north-1" {
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.5.0"
+  env               = "main_backup"
+  kind              = "full"
+  bootstrap_version = var.bootstrap_version
+  vault_role        = "ae-node"
+  vault_addr        = var.vault_addr
+  node_config       = "secret/aenode/config/main_backup_full"
+  aeternity         = { package = "unused" }
+
+  static_nodes            = 1
+  spot_price              = "0.07"
+  instance_type           = "t3.medium"
+  ami_name                = "aeternity-ubuntu-18.04-v1616681631"
+  additional_storage      = true
+  additional_storage_size = 200
+
+  providers = {
+    aws = "aws.eu-north-1"
+  }
+}
+
+module "aws_deploy-main_backup_light-eu-north-1" {
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.5.0"
+  env               = "main_backup"
+  kind              = "light"
+  bootstrap_version = var.bootstrap_version
+  vault_role        = "ae-node"
+  vault_addr        = var.vault_addr
+  node_config       = "secret/aenode/config/main_backup_light"
+  aeternity         = { package = "unused" }
+
+  static_nodes            = 1
+  spot_price              = "0.07"
+  instance_type           = "t3.medium"
+  ami_name                = "aeternity-ubuntu-18.04-v1616681631"
+  additional_storage      = true
+  additional_storage_size = 200
+
+  providers = {
+    aws = "aws.eu-north-1"
+  }
+}
+
 resource "null_resource" "ips1" {
   triggers = {
     depends_on = "${join(",", module.aws_deploy-main-ap-southeast-1.static_node_ips)}"
