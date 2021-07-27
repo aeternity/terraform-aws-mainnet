fa1467cc03ded6107545e7ee3e83c42ac8447cf6
Dincho Todorov <dincho@users.noreply.github.com>
2021-07-27 13:14:26 +0300

    Remove a temporary fleet

--- main.tf
+++ main.tf
@@ -27,35 +27,6 @@ module "aws_deploy-main-ap-southeast-1" {
   }
 }
 
-module "aws_deploy-main-5-6-0-ap-southeast-1" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
-  env               = "main_5_6_0"
-  bootstrap_version = var.bootstrap_version
-  vault_role        = "ae-node"
-  vault_addr        = var.vault_addr
-
-  static_nodes   = 1
-  spot_nodes_min = 0
-  spot_nodes_max = 0
-
-  spot_price       = "0.15"
-  instance_type    = "t3.large"
-  ami_name         = "aeternity-ubuntu-18.04-v1616681631"
-  root_volume_size = 40
-
-  additional_storage      = true
-  additional_storage_size = 200
-  snapshot_filename       = "mnesia_main_v-1_latest.tgz"
-
-  aeternity = {
-    package = var.package
-  }
-
-  providers = {
-    aws = "aws.ap-southeast-1"
-  }
-}
-
 module "aws_deploy-main-eu-north-1" {
   source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
   env               = "main"
