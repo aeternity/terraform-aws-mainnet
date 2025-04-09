3c7cafaf4872b49fdb05027d21f86cd22e82c596
Dincho Todorov <dincho@users.noreply.github.com>
2025-04-09 12:49:10 +0300

    Remove us-east-2

--- main.tf
+++ main.tf
@@ -91,36 +91,36 @@ module "aws_deploy-main-us-west-2" {
   }
 }
 
-module "aws_deploy-main-us-east-2" {
-  source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
-  env    = "main"
+# module "aws_deploy-main-us-east-2" {
+#   source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
+#   env    = "main"
 
-  static_nodes = 3
+#   static_nodes = 3
 
-  instance_type  = "t3.large"
-  instance_types = ["t3.large", "c5.large", "c6i.large"]
-  ami_name       = "aeternity-ubuntu-22.04-v1709639419"
+#   instance_type  = "t3.large"
+#   instance_types = ["t3.large", "c5.large", "c6i.large"]
+#   ami_name       = "aeternity-ubuntu-22.04-v1709639419"
 
-  root_volume_size        = 40
-  additional_storage      = true
-  additional_storage_size = 200
+#   root_volume_size        = 40
+#   additional_storage      = true
+#   additional_storage_size = 200
 
-  tags = {
-    role = "aenode"
-    env  = "main"
-  }
+#   tags = {
+#     role = "aenode"
+#     env  = "main"
+#   }
 
-  config_tags = {
-    vault_role        = "ae-node"
-    vault_addr        = var.vault_addr
-    bootstrap_version = var.bootstrap_version
-    bootstrap_config  = "secret2/aenode/config/main"
-  }
+#   config_tags = {
+#     vault_role        = "ae-node"
+#     vault_addr        = var.vault_addr
+#     bootstrap_version = var.bootstrap_version
+#     bootstrap_config  = "secret2/aenode/config/main"
+#   }
 
-  providers = {
-    aws = aws.us-east-2
-  }
-}
+#   providers = {
+#     aws = aws.us-east-2
+#   }
+# }
 
 # Monitoring nodes
 
@@ -156,69 +156,69 @@ module "aws_deploy-main_mon-eu-north-1" {
   }
 }
 
-module "aws_deploy-main_backup_full-eu-north-1" {
-  source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
-  env    = "main_backup"
+# module "aws_deploy-main_backup_full-eu-north-1" {
+#   source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
+#   env    = "main_backup"
 
-  static_nodes = 1
+#   static_nodes = 1
 
-  instance_type  = "t3.medium"
-  instance_types = ["t3.medium", "t3.large", "c5.large", "m5.large"]
-  ami_name       = "aeternity-ubuntu-22.04-v1709639419"
+#   instance_type  = "t3.medium"
+#   instance_types = ["t3.medium", "t3.large", "c5.large", "m5.large"]
+#   ami_name       = "aeternity-ubuntu-22.04-v1709639419"
 
-  root_volume_size        = 24
-  additional_storage      = true
-  additional_storage_size = 1000
+#   root_volume_size        = 24
+#   additional_storage      = true
+#   additional_storage_size = 1000
 
-  tags = {
-    role = "aenode"
-    env  = "main_backup"
-    kind = "full"
-  }
+#   tags = {
+#     role = "aenode"
+#     env  = "main_backup"
+#     kind = "full"
+#   }
 
-  config_tags = {
-    vault_role        = "ae-node"
-    vault_addr        = var.vault_addr
-    bootstrap_version = var.bootstrap_version
-    bootstrap_config  = "secret2/aenode/config/main_backup_full"
-  }
+#   config_tags = {
+#     vault_role        = "ae-node"
+#     vault_addr        = var.vault_addr
+#     bootstrap_version = var.bootstrap_version
+#     bootstrap_config  = "secret2/aenode/config/main_backup_full"
+#   }
 
-  providers = {
-    aws = aws.eu-north-1
-  }
-}
+#   providers = {
+#     aws = aws.eu-north-1
+#   }
+# }
 
-module "aws_deploy-main_backup_light-eu-north-1" {
-  source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
-  env    = "main_backup"
+# module "aws_deploy-main_backup_light-eu-north-1" {
+#   source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
+#   env    = "main_backup"
 
-  static_nodes = 1
+#   static_nodes = 1
 
-  instance_type  = "t3.medium"
-  instance_types = ["t3.medium", "t3.large", "c5.large", "m5.large"]
-  ami_name       = "aeternity-ubuntu-22.04-v1709639419"
+#   instance_type  = "t3.medium"
+#   instance_types = ["t3.medium", "t3.large", "c5.large", "m5.large"]
+#   ami_name       = "aeternity-ubuntu-22.04-v1709639419"
 
-  root_volume_size        = 24
-  additional_storage      = true
-  additional_storage_size = 1000
+#   root_volume_size        = 24
+#   additional_storage      = true
+#   additional_storage_size = 1000
 
-  tags = {
-    role = "aenode"
-    env  = "main_backup"
-    kind = "light"
-  }
+#   tags = {
+#     role = "aenode"
+#     env  = "main_backup"
+#     kind = "light"
+#   }
 
-  config_tags = {
-    vault_role        = "ae-node"
-    vault_addr        = var.vault_addr
-    bootstrap_version = var.bootstrap_version
-    bootstrap_config  = "secret2/aenode/config/main_backup_light"
-  }
+#   config_tags = {
+#     vault_role        = "ae-node"
+#     vault_addr        = var.vault_addr
+#     bootstrap_version = var.bootstrap_version
+#     bootstrap_config  = "secret2/aenode/config/main_backup_light"
+#   }
 
-  providers = {
-    aws = aws.eu-north-1
-  }
-}
+#   providers = {
+#     aws = aws.eu-north-1
+#   }
+# }
 
 resource "null_resource" "ips1" {
   triggers = {
@@ -238,8 +238,8 @@ resource "null_resource" "ips3" {
   }
 }
 
-resource "null_resource" "ips4" {
-  triggers = {
-    depends_on = join(",", module.aws_deploy-main-us-east-2.static_node_ips)
-  }
-}
+# resource "null_resource" "ips4" {
+#   triggers = {
+#     depends_on = join(",", module.aws_deploy-main-us-east-2.static_node_ips)
+#   }
+# }
